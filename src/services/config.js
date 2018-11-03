97c366f71d5dcdee5cf9cdbc2f7011e472520cda
minh <minh.trinh@dynamicly.com>
2018-11-03 14:01:20 -0400

    add services

---
+++ src/services/config.js
@@ -0,0 +1,10 @@
+export default config = {
+  github: {
+    base: 'https://api.github.com',
+    
+  },
+  gitlab: {
+    base: 'https://gitlab.com/api/v4',
+    token: 'sySs66YmvPVXy_Yuxgrc'
+  }
+};
