87807fc8a746ab08a66df4b3e5c8b6545d1f1d4e
David Tran <gvdavid.tran@gmail.com>
2018-11-03 17:20:25 -0400

    added more columns to history

--- src/components/CommitHistory.js
+++ src/components/CommitHistory.js
@@ -5,11 +5,13 @@ class CommitHistory extends React.Component {
     return (
       <div class="section">
         <h3>My Commit History</h3>
-        <table>
+        <table class="striped">
           <thead>
             <tr>
               <th>Date</th>
-              <th>Number of commits</th>
+              <th>Number of commits on Github</th>
+              <th>Number of commits on Gitlab</th>
+              <th>Total number of commits</th>
             </tr>
           </thead>
 
@@ -17,13 +19,19 @@ class CommitHistory extends React.Component {
             <tr>
               <td>September 1</td>
               <td>3</td>
+              <td>3</td>
+              <td>9</td>
             </tr>
             <tr>
               <td>September 2</td>
               <td>1</td>
+              <td>4</td>
+              <td>5</td>
             </tr>
             <tr>
               <td>September 3</td>
+              <td>5</td>
+              <td>0</td>
               <td>5</td>
             </tr>
           </tbody>
