ff50bffcd0206b0d3121c4403bf8dd61a7ee1b28
Tatum Alenko <tatum.alenko@gmail.com>
2018-11-04 16:54:14 -0500

    Fix deployment bug

--- .config/config.js
+++ .config/config.js
@@ -1,10 +1,14 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   github: {
     base: 'https://api.github.com',
-    token: process.env.GITHUB_TOKEN,
+    token: process.env.GATSBY_GITHUB_TOKEN,
   },
   gitlab: {
     base: 'https://gitlab.com/api/v4',
-    token: process.env.GITLAB_TOKEN,
+    token: process.env.GATSBY_GITLAB_TOKEN,
   },
 };
